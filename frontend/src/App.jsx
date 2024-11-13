90717124fc69b7e1aab5230070983b031b821e0f
Lashkin <lashkinsv@gmail.com>
2024-11-13 20:31:42 +0300

    Add Dockerfile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,31 @@ function App() {
   // Определение состояния для массива объектов items
   const [items, setItems] = useState(null);
 
+  async function fetchData() {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      if (!response.ok) {
+        throw new Error(`Ошибка: ${response.status} ${response.statusText}`);
+      }
+      const data = await response.json();
+      setItems(data);
+    } catch (error) {
+      console.error("Ошибка:", error.message);
+    }
+  }
+
   const fetchPosts = () => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((json) => setItems(json));
   };
 
+  //console.log(fetchData());
+
   useEffect(() => {
-    fetchPosts();
-    setInterval(() => {
-      fetchPosts();
-    }, 2000);
+    fetchData();
   }, []);
 
   const listItems =
@@ -27,7 +41,7 @@ function App() {
         </p>
       </li>
     ));
- 
+
   return <ul>{listItems}</ul>;
 }
 
