43c27631c3bb0535f24e551ecb2ec22d5d2f8e36
Lashkin <lashkinsv@gmail.com>
2024-11-13 11:24:39 +0300

    add vite to project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,10 @@ function App() {
   return (
     <div className="App">
       <p>Lorem</p>
+      <input></input>
+      <button>Нажми меня</button>
     </div>
-  )
+  );
 }
 
-
-export default App
+export default App;
